30c921848b4c03e0ea700a047433ea710b86838b
Tanmay Agrawal <tanmay7270@gmail.com>
2018-11-07 00:10:07 +0530

    nt2 api file

---
+++ caption.py
@@ -0,0 +1,29 @@
+import requests
+import nltk
+import random
+import sys
+
+path = sys.argv[1]
+r = requests.post(
+    "https://api.deepai.org/api/neuraltalk",
+    files={
+        'image': open(path, 'rb'),
+    },
+    headers={'api-key': '8a8842f4-e2fc-45f4-b2b9-4c24903cca76'}
+)
+TEXT = r.json()
+TEXT = TEXT['output']
+print(TEXT)
+# TEXT = nltk.corpus.gutenberg.words('austen-emma.txt')
+bigrams = nltk.bigrams(TEXT)
+cfd = nltk.ConditionalFreqDist(bigrams)
+
+# pick a random word from the corpus to start with
+word = random.choice(TEXT)
+# generate 15 more words
+for i in range(15):
+    print(word, end="")
+    if word in cfd:
+        word = random.choice(list(cfd[word]))
+    else:
+        break
